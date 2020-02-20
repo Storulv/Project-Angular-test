a76aec2bf860d76230b2cda33b0d52c5ad3728ad
Storulv <jordycorvisiercontact@gmail.com>
2020-02-20 18:42:19 +0100

    String/Property/Event/two-way binding

--- src/app/mon-premier/mon-premier.component.ts
+++ src/app/mon-premier/mon-premier.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mon-premier.component.scss']
 })
 export class MonPremierComponent implements OnInit {
+  title = 'Angular Project One';
+  isAuth = false;
 
-  constructor() { }
+  appareilOne = 'TV';
+  appareilTwo = 'PC';
+  appareilThree = 'Machine Spécial Thé';
+
+  constructor() {
+    setTimeout(() => {
+      this.isAuth = true;
+    }, 4000 );
+  }
+
+  onAllumer() {
+    console.log('On allume tout !');
+  }
 
   ngOnInit(): void {
   }
-
 }
